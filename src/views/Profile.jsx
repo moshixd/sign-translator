5da3b3ee7f8e6ad318fa60e1da82070503a4d2f6
moshixd <patrick_hajiyahya124@outlook.com>
2022-07-26 14:42:27 +0200

    demo coffe app

---
+++ src/views/Profile.jsx
@@ -0,0 +1,5 @@
+const Profile = () => {
+  return <h1>ProfilePage</h1>;
+};
+
+export default Profile;
---
+++ src/views/Translation.jsx
@@ -0,0 +1,5 @@
+const Translation = () => {
+  return <h1>TranslationPage</h1>;
+};
+
+export default Translation;
